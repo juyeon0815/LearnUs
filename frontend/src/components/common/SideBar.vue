4003bcfa6f8f982d22c74f2ed4d9470ed5e39e96
이세라 <you@example.com>
2021-10-25 16:33:48 +0900

    [S05P31A306-36] Feat : 사이드바 생성

--- frontend/src/components/common/SideBar.vue
+++ frontend/src/components/common/SideBar.vue
@@ -1,6 +1,42 @@
 <template>
   <div class="side-bar">
-    사이드바
+    <div class="logo">
+      <i class="fi fi-rr-mountains"></i>
+      <span>LOGO</span>
+    </div>
+    <div class="menu">
+      <div 
+        class="menu-item"
+        @click="$router.push('/')"
+      >
+        <i class="fi fi-sr-home"></i>
+        <span>HOME</span>
+      </div>
+      <div class="menu-item">
+        <i class="fi fi-sr-play-alt"></i>
+        <span>ON<span class="t-orange">:</span>AIR</span>
+      </div>
+      
+      <div class="menu-item">
+        <i class="fi fi-rr-film"></i>
+        <span>RE<span class="t-orange">:</span>PLAY</span>
+        <div class="category">
+          <span>‣ 전체보기</span>
+          <span>‣ 1학기 과정</span>
+          <span>‣ 2학기 과정</span>
+        </div>
+      </div>
+      <div class="menu-item">
+        <i class="fi fi-rr-sunrise"></i>
+        <span><span class="t-orange">:</span>ADMIN</span>
+        <div class="category">
+          <span>‣ 교육생 관리</span>
+          <span>‣ 교육 영상 관리</span>
+          <span>‣ 라이브 생성</span>
+          <span>‣ 교육생 관리</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
--- frontend/src/views/Index.vue
+++ frontend/src/views/Index.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="index">
     <SideBar/>
+    <TopBar/>
   </div>
 </template>
 
 <script>
 import SideBar from '@/components/common/SideBar'
+import TopBar from '@/components/common/TopBar'
 
 export default {
   name: 'Index',
   components: {
     SideBar,
+    TopBar,
   }
 }
 </script>
