a18aac55d8f536d1ea964dd364d828cc60590d8f
이세라 <you@example.com>
2021-10-26 17:13:38 +0900

    [S05P31A306-49] Feat : Index 쿠폰 당첨자 공지판 생성

---
+++ frontend/src/components/index/WinnerList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="winner-list">
+    <div class="chapter">Congratulations<span class="t-orange">:</span></div>
+    <div class="content">
+      <div class="title">오늘의 퀴즈왕</div>
+      <div class="grid-2">
+        <WinnerListItem
+          v-for="idx in 5"
+          :key="idx"
+        />
+      </div>
+      <div class="title">오늘의 참여왕</div>
+      <div class="grid-2">
+        <WinnerListItem
+          v-for="idx in 10"
+          :key="idx"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import WinnerListItem from './WinnerListItem.vue'
+export default {
+  name: 'WinnerList',
+  components: {
+    WinnerListItem
+  }
+}
+</script>
+
+<style>
+
+</style>
