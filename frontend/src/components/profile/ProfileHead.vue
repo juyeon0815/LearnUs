7f270ea5a1b56eb174c6d6501acf3fad36709c9e
ssafy-mann <6513032@gmail.com>
2021-10-27 17:26:28 +0900

    [S05P31A306-51] Feat : 프로필 사진 업데이트 컴포넌트 레이아웃 작업 완료

--- frontend/src/components/profile/ProfileHead.vue
+++ frontend/src/components/profile/ProfileHead.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="profile-head">
-    <p>ProfileHead</p>
+    <!-- 사진 관리 -->
+    <div class="student-photo">
+      <div class="photo-circle"></div>
+      <div class="photo-update">
+        <span class="text-btn">변경</span>
+        <span class="dot-space">⬝</span>
+        <span class="text-btn">삭제</span>
+        </div>
+    </div>
+    <!-- 이름, 학번 및 소속 -->
+    <div class="student-info">
+      <div>
+        <span class="student-name">우만승</span>
+        <span class="student-id">0545052</span>
+      </div>
+      <div class="department">
+        <span class="grade">SSAFY 5기 교육생</span>
+        <span class="region-class">(서울 3반)</span>
+      </div>
+    </div>
   </div>
 </template>
 
