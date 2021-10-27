f44022b57c166c258f6d305e5678fd1276cd1ae7
이세라 <you@example.com>
2021-10-27 16:41:29 +0900

    [S05P31A306-79] Feat : 상단 로고 추가 컴포넌트 추가

---
+++ frontend/src/components/common/TopLogo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="top-logo">
+    <img 
+      src="@/assets/image/logo/logo-ko.svg" 
+      alt=""
+      @click="$router.push('/')"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopLogo'
+}
+</script>
+
+<style>
+
+</style>
