67592f7a49f6b402df246e7a6da959623660fd4c
ssafy-mann <6513032@gmail.com>
2021-10-28 16:06:22 +0900

    [S05P31A306-53] Feat: 회원 정보 수정 html/css 작업

--- frontend/src/components/profile/ProfileInfo.vue
+++ frontend/src/components/profile/ProfileInfo.vue
@@ -1,6 +1,31 @@
 <template>
   <div class="profile-info">
-    <p>ProfileInfo</p>
+    <div class="title-button-box">
+      <div class="title">
+        <span>My Info</span>
+      </div>
+      <button class="change-info-btn">
+        정보수정
+      </button>
+    </div>
+    <div class="info-inputs">
+      <!-- 닉네임 input -->
+      <div class="info-input-box">
+        <input 
+          type="text"
+          class="info-input"
+        />
+        <label>Nickname</label>
+      </div>
+      <!-- 전화번호 input -->
+      <div class="info-input-box">
+        <input 
+          type="text"
+          class="info-input"
+        />
+        <label>Phone Number</label>
+      </div>
+    </div>
   </div>
 </template>
 
