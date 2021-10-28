155e73af6b847671a5bdaddeea6ceee6bc6c1118
이세라 <you@example.com>
2021-10-28 11:55:49 +0900

    [S05P31A306-81] Feat : JWT 토큰 처리를 위한 Axios 인터셉터 기본 세팅

--- frontend/src/store/modules/account.js
+++ frontend/src/store/modules/account.js
@@ -1,13 +1,20 @@
+import router from '@/router'
+
 const state = {
-  
+  accessToken: null,
 }
 
 const actions = {
-
+  onLogout ({ commit }) {
+    commit('SET_ACCESS_TOKEN', null)
+    router.push('/account/login')
+  }
 }
 
 const mutations = {
-  
+  SET_ACCESS_TOKEN (state, payload) {
+    state.accessToken = payload
+  }
 }
 
 const getters = {
