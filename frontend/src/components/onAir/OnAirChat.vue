bfc8bf9040570959367df84896f424c38735a872
이세라 <you@example.com>
2021-10-29 12:05:46 +0900

    [S05P31A306-64] Feat : 출석체크 팝업창 추가

--- frontend/src/components/onAir/OnAirChat.vue
+++ frontend/src/components/onAir/OnAirChat.vue
@@ -1,19 +1,37 @@
 <template>
-  <div class="on-air-chat">
+  <div id="chat-box" class="on-air-chat">
+    <AttendCheck/>
     <OnAirChatList/>
-    <OnAirChatInput/>
+    <OnAirChatInput @autosize="autosizeList"/>
   </div>
 </template>
 
 <script>
+import './onAirChat.scss'
+import AttendCheck from './chat/AttendCheck.vue'
 import OnAirChatInput from './chat/OnAirChatInput.vue'
 import OnAirChatList from './chat/OnAirChatList.vue'
 export default {
   name: 'OnAirChat',
   components: {
     OnAirChatInput,
-    OnAirChatList
+    OnAirChatList,
+    AttendCheck
+  },
+  methods: {
+    autosizeList () {
+      const chat = document.getElementById('chat-box')
+      const chatList = document.getElementById('chat-list')
+      const chatInput = document.getElementById('chat-input')
+      console.log(chatInput.offsetHeight)
+      chatList.style.height = (chat.offsetHeight - chatInput.offsetHeight) + "px"
+    }
+  },
+  mounted () {
+    this.autosizeList()
+    window.addEventListener('resize', this.autosizeList())
   }
+
 }
 </script>
 
