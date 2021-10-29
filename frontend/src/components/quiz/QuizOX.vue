8e117898edcb596c5ea9d293455d025d2945a64c
이세라 <you@example.com>
2021-10-29 17:11:07 +0900

    [S05P31A306-65] Feat : 퀴즈 팝업 UI 작업

---
+++ frontend/src/components/quiz/QuizOX.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="quiz-ox">
+    <span>O</span>
+    <span>X</span>
+  </div>
+</template>
+
+<script>
+import './quiz.scss'
+export default {
+  name: 'QuizOX',
+}
+</script>
+
+<style>
+
+</style>
