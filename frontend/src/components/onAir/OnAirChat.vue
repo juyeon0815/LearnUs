4a69ad1eac9a76edfa74fabfd5fb76da51ffcb93
이세라 <you@example.com>
2021-10-29 17:11:26 +0900

    [S05P31A306-65] Feat : 퀴즈 팝업 채팅창에 추가

--- frontend/src/components/onAir/OnAirChat.vue
+++ frontend/src/components/onAir/OnAirChat.vue
@@ -2,6 +2,7 @@
   <div id="chat-box" class="on-air-chat">
     <AttendCheck v-if="attend"/>
     <AttendResult v-if="attendResult"/>
+    <SolvingQuiz v-if="quiz"/>
     <OnAirChatList/>
     <OnAirChatInput @autosize="autosizeList"/>
   </div>
@@ -11,6 +12,7 @@
 import './onAirChat.scss'
 import AttendCheck from './chat/AttendCheck.vue'
 import AttendResult from './chat/AttendResult.vue'
+import SolvingQuiz from './chat/SolvingQuiz.vue'
 import OnAirChatInput from './chat/OnAirChatInput.vue'
 import OnAirChatList from './chat/OnAirChatList.vue'
 export default {
@@ -19,26 +21,32 @@ export default {
     OnAirChatInput,
     OnAirChatList,
     AttendCheck,
-    AttendResult
+    AttendResult,
+    SolvingQuiz
   },
   data () {
     return {
       attend: false,
-      attendResult: false
+      attendResult: true,
+      quiz: false,
     }
   },
   methods: {
-    autosizeList () {
+    autosize () {
       const chat = document.getElementById('chat-box')
       const chatList = document.getElementById('chat-list')
       const chatInput = document.getElementById('chat-input')
+      const pb = document.getElementById('popup-b')
       chatList.style.height = (chat.offsetHeight - chatInput.offsetHeight) + "px"
       chatList.scrollTop = chatList.scrollHeight
+      if (pb) {
+        pb.style.bottom = chatInput.offsetHeight + 'px'
+      }
     }
   },
   mounted () {
-    this.autosizeList()
-    window.addEventListener('resize', this.autosizeList())
+    this.autosize()
+    window.addEventListener('resize', this.autosize())
   }
 
 }
