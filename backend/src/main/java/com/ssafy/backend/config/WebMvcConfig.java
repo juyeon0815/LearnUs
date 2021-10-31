76a48e90f4e4a397f52791339790376fef7f764c
류희영 <dbgmldud8313@naver.com>
2021-10-31 17:00:53 +0900

    [S05P31A306-42] Fix : addInterceptors 수정

--- backend/src/main/java/com/ssafy/backend/config/WebMvcConfig.java
+++ backend/src/main/java/com/ssafy/backend/config/WebMvcConfig.java
@@ -16,8 +16,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(jwtInterceptor).addPathPatterns(Arrays.asList("/user/**", "/mattermost/**", "/schedule/**", "/track/**"))
-                .excludePathPatterns(Arrays.asList("/user/**", "/broadcast/**", "/user/searchId/{userId}/{phone}", "/user/searchPW/{email}/{userId}/{phone}", "/user/resetPW"));
+        registry.addInterceptor(jwtInterceptor).addPathPatterns(Arrays.asList("/user/**", "broadcast/**", "/mattermost/**", "/schedule/**", "/track/**"))
+                .excludePathPatterns(Arrays.asList("/user/login", "/user/searchId/{userId}/{phone}", "/user/searchPW/{email}/{userId}/{phone}", "/user/resetPW"));
     }
 
     @Override
