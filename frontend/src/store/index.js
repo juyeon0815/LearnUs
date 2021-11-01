a131e226e739ea95800b61c645046eed3976f206
이세라 <you@example.com>
2021-11-01 14:07:06 +0900

    Rename : Vuex 모듈 이름 변경

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,17 @@
 import { createStore } from 'vuex'
 import createPersistedState from "vuex-persistedstate";
 import account from './modules/account'
+import admin from './modules/admin'
 import chat from './modules/chat'
-import live from './modules/live'
+import video from './modules/video'
 
 const store = createStore({
   plugins: [createPersistedState()],
   modules: {
     account,
+    admin,
     chat,
-    live
+    video
   }
 })
 
