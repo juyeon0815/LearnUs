2243084fb70d7da063710cfeab861fe44250289c
류희영 <dbgmldud8313@naver.com>
2021-11-03 12:46:37 +0900

    [S05P31A306-144] Fix : Track 관련 전체 수정

---
+++ backend/src/main/java/com/ssafy/backend/service/TrackSubjectService.java
@@ -0,0 +1,16 @@
+package com.ssafy.backend.service;
+
+import com.ssafy.backend.dto.TrackSubject;
+import com.ssafy.backend.dto.TrackSubjectInfo;
+
+import java.util.List;
+
+public interface TrackSubjectService {
+    public List<String> getOrdinalNo();
+    public boolean insert(TrackSubjectInfo trackSubjectInfo);
+    public boolean update(TrackSubjectInfo trackSubjectInfo);
+    public boolean delete(int trackSubjectId);
+    public List<TrackSubject> getTrackSubjectAll();
+    public TrackSubject getCurrentTrackSubject();
+    public boolean currentTrackSubjectUpdate(String newSubjectName);
+}
