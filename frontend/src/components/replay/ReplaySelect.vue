8e6da0346d50aaa0c957d06342b841641ada5a6f
juyeon0815 <wndusx1@gmail.com>
2021-11-08 15:40:10 +0900

    Feat : 트랙별 리스트 작업

---
+++ frontend/src/components/replay/ReplaySelect.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="track-current">
+    <div class="select-box">
+      <select 
+        class="ordinal-select"
+        v-model="this.trackList[0].trackName"
+        @change="changeSubject"
+      >
+        <option 
+          class="decorated"
+          v-for="track in trackList"
+          :value="track.trackName"
+          :key="track.trackId"
+        >{{ track.trackName }}</option>
+      </select>
+  </div>  
+    </div>
+    {{param}}
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'TrackInfo',
+ data: () => {
+    return {
+      subjectId: null,
+      userInfo: null,
+      subjectName: '',
+      category : ''
+    }
+  },
+  methods: {
+    
+  },
+  computed: {
+    ...mapState(
+      'admin', ['tracks']
+    ),
+    trackList () {
+     return this.tracks.filter(track => {
+        return track.trackSubject.subjectName === this.$route.params.category;
+      }) 
+    }
+  },
+}
+
+</script>
+
+<style>
+
+</style>
