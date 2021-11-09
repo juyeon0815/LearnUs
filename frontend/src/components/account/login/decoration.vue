f31523179b4d7e999b79600c23687868f82908ec
이세라 <you@example.com>
2021-11-09 20:08:28 +0900

    Design : 로그인 페이지 디자인 수정

--- frontend/src/components/account/login/decoration.vue
+++ frontend/src/components/account/login/decoration.vue
@@ -1,27 +1,15 @@
 <template>
   <div class="deco-account">
-    <div class="ground"></div>
-    <div class="deco-img">
-      <img 
-        src="@/assets/image/logo/mountain-w500.svg" 
-        alt="mountain"
-        class="mountain"
-      >
-      <img 
-        src="@/assets/image/logo/rocket-w500.svg" 
-        alt="mountain"
-        class="rocket right"
-      >
-      <img 
-        src="@/assets/image/logo/rocket-w500.svg" 
-        alt="mountain"
-        class="rocket left"
-      >
-    </div>
-    <div class="title-box">
-      <span class="subtitle">SSAFY Live Platform</span>
-      <span class="title">LearnUs</span>
-    </div>
+    <img 
+      src="@/assets/image/deco/login.jpg" 
+      alt="mountain"
+      class="deco-lg"
+    >
+    <img 
+      src="@/assets/image/deco/login-sm.jpg" 
+      alt="mountain"
+      class="deco-sm"
+    >
   </div>
 </template>
 
