cd1550cf998a74b9550e091b79f8b252234d9087
juyeon0815 <wndusx1@gmail.com>
2021-11-10 10:23:57 +0900

    Rename : TrackInfo -> ReplaySelect로 변경

--- frontend/src/views/replay/Replay.vue
+++ frontend/src/views/replay/Replay.vue
@@ -2,8 +2,8 @@
   <div class="replay">
     <div class="row">
       <span class="title">RE<span class="colon">:</span>PLAY</span>
+      <ReplaySelect v-if="this.$route.params.category != 'all'" />
     </div>
-    <TrackInfo v-if="this.$route.params.category != 'all'" />
     <ReplayList />
   </div>
 </template>
@@ -12,7 +12,7 @@
 import "./replay.scss";
 
 import ReplayList from "@/components/replay/ReplayList";
-import TrackInfo from "@/components/replay/ReplaySelect";
+import ReplaySelect from "@/components/replay/ReplaySelect";
 
 export default {
   name: "Replay",
@@ -23,7 +23,7 @@ export default {
   },
   components: {
     ReplayList,
-    TrackInfo,
+    ReplaySelect,
   },
   watch: {
     $route() {
