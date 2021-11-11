b9f07c4f9c408eb4ab68b4c1b07bdda6b4461547
류희영 <dbgmldud8313@naver.com>
2021-11-11 18:52:20 +0900

    [S05P31A306-178] Fix : EC2 RabbitMQ 연결 버그

--- backend/src/main/java/com/ssafy/backend/config/RabbitConfig.java
+++ backend/src/main/java/com/ssafy/backend/config/RabbitConfig.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -25,20 +24,6 @@ public class RabbitConfig {
 
     @Value("${spring.rabbitmq.host}")
     private String host;
-
-    @Value("${spring.rabbitmq.port}")
-    private int port;
-
-    private static final String ROUTING_KEY = "broadcast.*";
-
-    // messageConverter를 커스터마이징 하기 위해 Bean 새로 등록
-    @Bean
-    public RabbitTemplate rabbitTemplate() {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
-        rabbitTemplate.setMessageConverter(jsonMessageConverter());
-        rabbitTemplate.setRoutingKey(ROUTING_KEY);
-        return rabbitTemplate;
-    }
 
     // spring 에서 자동생성해주는 connectionFactory는 SimpleConnectionFactory인가? 그건데
     // 여기서 사용하는 건 cachingConnectionFactory라 새로 등록해줌
