5596f3b6cfdb6517b9fea98526e5ed10b0f47ca5
이세라 <you@example.com>
2021-11-11 10:45:30 +0900

    Feat : 스튜디오 기프티콘 관리 컴포넌트 생성

--- frontend/src/components/onAir/OnAirControl.vue
+++ frontend/src/components/onAir/OnAirControl.vue
@@ -12,12 +12,17 @@
           @click="changeControl(0)"
         >교육생 관리</span>
         <span 
-          :class="[control ? 'active' : '', 'tab-item']" 
+          :class="[control === 1 ? 'active' : '', 'tab-item']" 
           @click="changeControl(1)"
         >퀴즈 관리</span>
+        <span 
+          :class="[control === 2 ? 'active' : '', 'tab-item']" 
+          @click="changeControl(2)"
+        >기프티콘</span>
       </div>
       <ControlStudent v-if="!control"/>
-      <ControlQuiz v-else/>
+      <ControlQuiz v-else-if="control === 1"/>
+      <ControlGifticon v-else-if="control === 2"/>
     </div>
   </div>
 </template>
@@ -28,12 +33,14 @@ import VideoInfo from './studio/VideoInfo.vue'
 import VideoPreview from './studio/VideoPreview.vue'
 import ControlStudent from './studio/ControlStudent.vue'
 import ControlStudentTable from './studio/ControlStudentTable.vue'
+import ControlGifticon from './studio/ControlGifticon.vue'
 import ControlQuiz from './studio/ControlQuiz.vue'
 export default {
   name: 'OnAirControl',
   components: {
     VideoInfo,
     VideoPreview,
+    ControlGifticon,
     ControlStudent,
     ControlStudentTable,
     ControlQuiz
---
+++ frontend/src/components/onAir/studio/ControlGifticon.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="control-gifticon">
+    기프티콘 페이지입니당
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ControlGifticon',
+}
+</script>
+
+<style>
+
+</style>
