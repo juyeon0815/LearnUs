d9457d23fe7aabb05ca13d405cf9bf67c8e3719e
ssafy-mann <6513032@gmail.com>
2021-11-12 09:25:36 +0900

    Feat : 기프티콘 초반 작업

--- frontend/src/components/onAir/studio/ControlGifticon.vue
+++ frontend/src/components/onAir/studio/ControlGifticon.vue
@@ -1,12 +1,83 @@
 <template>
   <div class="control-gifticon">
-    기프티콘 페이지입니당
+    <div class="student-table">
+        <table>
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>이름</th>
+              <th>학번</th>
+              <th>분반</th>
+              <th>출석 여부</th>
+            </tr>
+          </thead>
+          <tbody>
+            <ControlStudentTableRow
+              v-for="(student, idx) in paginatedArea"
+              :key="idx"
+              :student="student"
+              :idx="idx + (currentPage - 1) * 10"
+            />
+          </tbody>
+        </table>
+        <div v-if="!gifticonList.length" class="empty">
+          검색 결과가 없습니다.
+        </div>
+
+        <ul class="pagination" v-if="totalPage && totalPage > 1">
+          <li
+            :class="{ disabled: currentPage === 1 }"
+            @click="changePage(currentPage - 1)"
+          >&laquo;</li>
+          <li
+            :class="{ active: page === currentPage }"
+            v-for="page in totalPage"
+            :key="page"
+            @click="changePage(page)"
+          >
+            {{ page }}
+          </li>
+          <li
+            :class="{ disabled: currentPage === totalPage }"
+            @click="changePage(currentPage + 1)"
+          >&raquo;</li>
+        </ul>
+      </div>
   </div>
 </template>
 
 <script>
+import { mapActions, mapState } from 'vuex'
+import ControlStudentTableRow from './ControlStudentRow.vue'
+
 export default {
   name: 'ControlGifticon',
+  components: {
+    ControlStudentTableRow,
+  },
+  data () {
+    return {
+      currentPage: 1,
+    }
+  },
+  methods: {
+    ...mapActions('gifticon', ['getGifticonList']),
+  },
+  created() {
+    this.getGifticonList(this.$route.params.id)
+  },
+  computed: {
+    ...mapState('gifticon', ['gifticonList']),
+    paginatedArea () {
+      let start = (this.currentPage - 1) * 10
+      let end = this.currentPage * 10
+      return this.gifticonList.slice(start, end)
+    },
+    totalPage () {
+      const total = this.gifticonList.length
+      return Math.ceil(total/10)
+    },
+  }
 }
 </script>
 
--- frontend/src/store/modules/gifticon.js
+++ frontend/src/store/modules/gifticon.js
@@ -1,25 +1,40 @@
-import router from '@/router'
 import gifticonApi from '@/api/gifticon'
 
 const state = {
-    gifticonList: null,
-    HttpStatus: null,
+  gifticonList: null,
 }
 
 const actions = {
-    async createGifticon ({ commit }, data) {
-        try {
-            const response = await gifticonApi.createGifticon(data)
-            commit('SET_HTTP_STATUS', response.status);
-            
-        } catch (err) {
-            console.log(err);
-        }
+  async createGifticon({ commit }, data) {
+    try {
+      const response = await gifticonApi.createGifticon(data)
+      console.log(response)
+      commit('TEST')
+    } catch (err) {
+      console.log(err);
     }
+  },
+  async getGifticonList({ commit }, broadcastId) {
+    try {
+      const response = await gifticonApi.getGifticonList(broadcastId)
+      if (response.status === 200) {
+        commit('SET_GIFTICON_LIST', response.data)
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  },
 }
 
 const mutations = {
-    SET_HTTP_STATUS (state, payload) {
-        state.httpStatus = payload
-    },
+  SET_GIFTICON_LIST(state, payload) {
+    state.gifticonList = payload
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  actions,
+  mutations,
 }
