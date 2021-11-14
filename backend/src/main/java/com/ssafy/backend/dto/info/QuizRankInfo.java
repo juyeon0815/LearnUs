ac9a44b4004c4578d3c5ca43832c8909aeffc7ca
류희영 <dbgmldud8313@naver.com>
2021-11-14 13:46:59 +0900

    Fix : 퀴즈 랭킹 불러올 때 객관식 리스트 추가

--- backend/src/main/java/com/ssafy/backend/dto/info/QuizRankInfo.java
+++ backend/src/main/java/com/ssafy/backend/dto/info/QuizRankInfo.java
@@ -2,6 +2,7 @@ package com.ssafy.backend.dto.info;
 
 import com.ssafy.backend.dto.Quiz;
 import com.ssafy.backend.dto.QuizAnswer;
+import com.ssafy.backend.dto.QuizSelect;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,4 +19,5 @@ public class QuizRankInfo {
     private List<QuizAnswer> quizAnswerList;
     private Map<Object, Integer> rateMap;
     private Quiz quiz;
+    private List<QuizSelect> quizSelectList;
 }
