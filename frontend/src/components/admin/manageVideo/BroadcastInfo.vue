2b653efc5132034fead317c4fdf3a3b039465373
이세라 <you@example.com>
2021-11-14 20:22:15 +0900

    Feat : 교육 영상 관리 페이지 작업

---
+++ frontend/src/components/admin/manageVideo/BroadcastInfo.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="broadcast-info">
+    <UpdateModal v-if="onUpdateModal" @hideModal="onUpdateModal = false"/>
+    <div class="default">
+      <div class="video">
+        <div class="player"></div>
+        <div class="btns">
+          <button
+            class="btn black"
+            @click="onUpdateModal = true"
+          >
+            <div class="btn-content">
+              <i class="fi fi-rr-pencil"></i>
+              <span>영상 정보 수정</span>
+            </div>
+          </button>
+          <button
+            class="btn black"
+          >
+            <div class="btn-content">
+              <i class="fi fi-rr-resources"></i>
+              <span>영상 소스 교체</span>
+            </div>
+          </button>
+          <button
+            class="btn black"
+          >
+            <div class="btn-content">
+              <i class="fi fi-rr-trash"></i>
+              <span>교육 영상 삭제</span>
+            </div>
+          </button>
+        </div>
+      </div>
+      <VideoInfo/>
+    </div>
+    <div class="chat"></div>
+  </div>
+</template>
+
+<script>
+// import VideoPlayer from '@/components/replay/video/ReplayVideoPlayer'
+import VideoInfo from './BroadcastVideoInfo.vue'
+import UpdateModal from '@/components/onAir/studio/VideoInfoUpdateModal'
+export default {
+  name: 'BroadcastInfo',
+  data () {
+    return {
+      onUpdateModal: false,
+    }
+  },
+  components: {
+    VideoInfo,
+    UpdateModal
+    // VideoPlayer
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/components/admin/manageVideo/QuizResultItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="quiz-item">
+    <div class="title">
+      <span class="num">Q{{ num+1 }}.</span>
+      <span class="question">{{ result.quiz.question }}</span>
+    </div>
+    <div class="result">
+      <div class="count">
+        <span class="val">{{ result.quiz.attendCount }}</span>
+        <span class="label">참가자 수</span>
+      </div>
+      <div class="count">
+        <span class="val">{{ result.quiz.answerCount }}</span>
+        <span class="label">정답자 수</span>
+      </div>
+      <div class="rank-box">
+        <div 
+          v-for="(item, idx) in result.quizAnswerList"
+          :key="idx"
+          class="user"
+        >
+          <span class="rank">{{ ranks[idx] }}</span>
+          <img class="profile" :src="item.user.profileUrl" alt="">
+          <span class="nickname">{{ item.user.nickname }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QuizResultItem',
+  props: {
+    result: Object,
+    num: Number
+  },
+  data () {
+    return {
+      ranks: ['1st', '2nd', '3rd']
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
