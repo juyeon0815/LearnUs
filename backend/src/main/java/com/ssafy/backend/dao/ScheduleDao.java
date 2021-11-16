5444ef048c570f2ee52d121078e8771570b2ebf3
류희영 <dbgmldud8313@naver.com>
2021-11-17 01:25:50 +0900

    Remove : Schedule 관련 삭제

--- backend/src/main/java/com/ssafy/backend/dao/ScheduleDao.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.backend.dao;
-
-import com.ssafy.backend.dto.Schedule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScheduleDao extends JpaRepository<Schedule, String> {
-    Schedule findScheduleByScheduleId(int scheduleId);
-}
