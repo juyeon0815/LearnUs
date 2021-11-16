04979b6113dde03960f266478160b609d4fa3902
이세라 <you@example.com>
2021-11-17 01:03:53 +0900

    Feat : 채팅창 관리자 표시

--- frontend/src/components/onAir/OnAirChat.vue
+++ frontend/src/components/onAir/OnAirChat.vue
@@ -57,15 +57,8 @@ export default {
           this.stomp.subscribe(
             `/exchange/chat.exchange/chat.${id}`,
             (message) => {
-              const payload = JSON.parse(message.body);
-              const data = {
-                userId: payload.userId,
-                nickName: payload.nickName,
-                profileUrl: payload.profileUrl,
-                message: payload.message,
-                regDate: payload.regDate
-              }
-              this.$store.commit('stomp/ADD_CHAT_LIST', data)
+              const payload = JSON.parse(message.body)
+              this.$store.commit('stomp/ADD_CHAT_LIST', payload)
               this.autosize()
             },
             { "auto-delete": true, durable: false, exclusive: false }
