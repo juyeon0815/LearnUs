8664ea866c47c7848cd93ed708bc77314f548f10
류희영 <dbgmldud8313@naver.com>
2021-11-17 00:13:06 +0900

    Fix : 방송 날짜로 정렬

--- backend/src/main/java/com/ssafy/backend/dao/BroadcastDao.java
+++ backend/src/main/java/com/ssafy/backend/dao/BroadcastDao.java
@@ -2,11 +2,11 @@ package com.ssafy.backend.dao;
 
 import com.ssafy.backend.dto.Broadcast;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface BroadcastDao extends JpaRepository<Broadcast, String> {
     Broadcast findBroadcastByBroadcastId(int broadcastId);
-    List<Broadcast> findBroadcastsByLiveCode(String liveCode);
-
+    List<Broadcast> findBroadcastsByLiveCodeOrderByBroadcastDateDesc(String liveCode);
 }
