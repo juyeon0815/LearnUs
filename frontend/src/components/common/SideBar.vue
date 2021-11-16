3e6e4d7e09f7da5dff82ef56eff6eac1c95c7cd5
이세라 <you@example.com>
2021-11-16 23:54:06 +0900

    Design : 스타일 수정

--- frontend/src/components/common/SideBar.vue
+++ frontend/src/components/common/SideBar.vue
@@ -24,7 +24,7 @@
         <i class="fi fi-rr-film" @click="moveToReplay(0, 0)"></i>
         <span @click="moveToReplay(0, 0)">RE<span class="t-orange">:</span>PLAY</span>
         <div v-if="optionReady" class="category">
-          <span v-if="!isAdmin" @click="moveToReplay(0, 0)">‣ 전체 보기</span>
+          <span v-if="!isAdmin" @click="moveToReplay(0, 0), changeReplayMenu(0)">‣ 전체 보기</span>
           <div v-if="!isAdmin">
             <span @click="changeReplayMenu(1)">‣ 1학기 과정</span>
             <transition name="fade"
