e3d38da4b4876e5dd3f4c75f418997da6c204bf3
ssafy-mann <6513032@gmail.com>
2021-11-16 15:18:54 +0900

    Feat : 인덱스 페이지 퀴즈왕, 참여왕 view, 컴포넌트 데이터 반영하도록 수정, ui 수정

--- frontend/src/components/index/WinnerList.vue
+++ frontend/src/components/index/WinnerList.vue
@@ -1,31 +1,54 @@
 <template>
   <div class="winner-list">
-    <div class="chapter">Congratulations<span class="t-orange">:</span></div>
-    <div class="content">
-      <div class="title">오늘의 퀴즈왕</div>
-      <div class="grid-2">
-        <WinnerListItem
-          v-for="idx in 5"
-          :key="idx"
-        />
+    <div class="chapter">Today's Ranking<span class="t-orange">:</span></div>
+    <div class="content" v-if="awardList">
+      <div class="content-item">
+        <div class="title">오늘의 퀴즈왕</div>
+        <div class="award">
+          <WinnerListItem
+            v-for="(award, idx) in awardList.quiz"
+            :award="award"
+            :key="idx"
+            :idx="idx"
+          />
+        </div>
       </div>
-      <div class="title">오늘의 참여왕</div>
-      <div class="grid-2">
-        <WinnerListItem
-          v-for="idx in 10"
-          :key="idx"
-        />
+      <div class="content-item">
+        <div class="title">오늘의 참여왕</div>
+        <div class="award">
+          <WinnerListItem
+            v-for="(award, idx) in awardList.chat"
+            :award="award"
+            :key="idx"
+            :idx="idx"
+          />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { mapActions, mapMutations, mapState } from 'vuex'
 import WinnerListItem from './WinnerListItem.vue'
+
 export default {
   name: 'WinnerList',
   components: {
     WinnerListItem
+  },
+  methods: {
+    ...mapActions('gifticon', ['getAwardList']),
+    ...mapMutations('gifticon', ['SET_AWARD_LIST']),
+  },
+  computed: {
+    ...mapState('gifticon', ['awardList'])
+  },
+  created() {
+    this.getAwardList()
+  },
+  unmounted() {
+    this.SET_AWARD_LIST(null)
   }
 }
 </script>
